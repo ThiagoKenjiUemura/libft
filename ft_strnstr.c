9066fd5c7fa30e9dad084ce869868edb8d10e1e2
Thiago Kenji Uemura <tkenji-u@student.42sp.org.br>
2025-07-16 18:10:33 -0300

    test

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: tkenji-u <tkenji-u@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 16:01:37 by tkenji-u          #+#    #+#             */
-/*   Updated: 2025/07/16 18:05:07 by tkenji-u         ###   ########.fr       */
+/*   Updated: 2025/07/16 18:10:14 by tkenji-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,8 @@ char 	*ft_strnstr(const char *big, const char *little, size_t len);
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-		while()
+		while(big != '\0')
+		{
+			
+		}
 }
