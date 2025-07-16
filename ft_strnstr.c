ed2076c9f0aa0794e41bb439bbc23c497d5217b1
Thiago Kenji Uemura <tkenji-u@c2r11p1.42sp.org.br>
2025-07-16 18:05:25 -0300

    funciton strnstr added

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,29 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkenji-u <tkenji-u@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/15 17:19:58 by tkenji-u          #+#    #+#             */
-/*   Updated: 2025/07/16 18:05:09 by tkenji-u         ###   ########.fr       */
+/*   Created: 2025/07/16 16:01:37 by tkenji-u          #+#    #+#             */
+/*   Updated: 2025/07/16 18:05:07 by tkenji-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-	
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-	size_t	i;
+char 	*ft_strnstr(const char *big, const char *little, size_t len);
 
-	i = 0;
-	while (s1[i] != '\0' || s2[i] != '\0' && i < n)
-	{
-		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		i++;
-	}
-	return (0);
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+		while()
 }
