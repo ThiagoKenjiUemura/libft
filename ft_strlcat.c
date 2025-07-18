a2608ff8b14c1d0d3cf6f944aca01647273b4250
Thiago Uemura <thiagouemura@MacBook-Air-de-Thiago.local>
2025-07-18 09:46:00 -0300

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -16,5 +16,18 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	
+    size_t  i;
+    size_t  j; 
+    size_t  dst_len;
+    size_t  src_len;
+
+    dst_len = ft_strlen(dst);
+    src_len = ft_strlen(src);
+
+    while (src[j] !='\0' && j < size - 1)
+    {
+        dst[i + j] = src[j];
+        j++;
+    }
+    return (i + j);
 }
