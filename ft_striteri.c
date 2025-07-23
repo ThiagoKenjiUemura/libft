3447152006180d9ee6c57fe076e8d764fb838925
Thiago Kenji Uemura <tkenji-u@student.42sp.org.br>
2025-07-23 20:04:32 -0300

    norma errors corrected

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,22 +6,22 @@
 /*   By: tkenji-u <tkenji-u@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 19:44:38 by tkenji-u          #+#    #+#             */
-/*   Updated: 2025/07/23 19:58:46 by tkenji-u         ###   ########.fr       */
+/*   Updated: 2025/07/23 20:04:10 by tkenji-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_striteri(char *s, void (*f)(unsigned int, char *))
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	unsigned int	i;;
-	
+	unsigned int	i;
+
 	if (!s || !f)
-		return;
+		return ;
 	i = 0;
 	while (s[i])
 	{
-		f(i , &s[i]);
+		f(i, &s[i]);
 		i++;
 	}
 }
